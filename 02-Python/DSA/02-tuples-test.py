d01db4bf5a18e4bcb5f93dccd3e807324169cb43
sajjadrahman56 <maxsajjad29@gmail.com>
2024-01-10 23:50:12 +0600

    set tuples and list

---
+++ 02-Python/DSA/02-tuples-test.py
@@ -0,0 +1,5 @@
+tuple_a = 1, 2
+tuple_b = (1, 2)
+
+print(tuple_a == tuple_b)
+print(tuple_a[1])
---
+++ 02-Python/DSA/03-set-.py
@@ -0,0 +1,12 @@
+# fruit = {"apple", "banana", "orange", "grapefruit"}  # define a set
+
+# print("watermelon" in fruit)  # check for element
+
+# fruit.add("watermelon")  # add an element
+# print(fruit)
+
+# print(fruit.pop())  # remove a random element
+# print(fruit)
+a = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+b = set(a)
+print(len(a) - len(b))
